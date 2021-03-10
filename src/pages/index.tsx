f38b0d0764f8dc79b62e9a660845ab92b258cc03
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-10 15:56:51 +0000

    Make job views responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,17 @@
-import Head from 'next/head';
 import { Button, JobView } from '../components';
 import { Job } from '../types';
 
 export default function Home({ jobs }: { jobs: Job[] }) {
   return (
     <>
-      <Head>
-        <title>Dev Jobs</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <h1 className='text-6xl text-center font-bold bg-blue-100 py-2'>
-        Dev Jobs
-      </h1>
-      <div className='px-6 pt-20'>
+      <div className='grid grid-cols-1 px-6 pt-20 gap-x-3 xl:gap-x-8 gap-y-16 sm:grid-cols-2 lg:grid-cols-3'>
         {jobs.map((job: Job) => (
           <JobView key={job.id} job={job} />
         ))}
       </div>
-      <div className='text-center mt-8 mb-16'>
-        <Button primary={true}>View More</Button>
+      <div className='mt-8 mb-16 text-center'>
+        <Button primary={true}>Load More</Button>
       </div>
-      <footer className='flex items-center justify-center w-full h-24 border-t'>
-        <a
-          href='https://josephgattuso.github.io'
-          className='flex items-center justify-center'
-        >
-          Powered by <img className='h-4 ml-2' src='/logo.svg' alt='' />
-        </a>
-      </footer>
     </>
   );
 }
