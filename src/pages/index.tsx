7b1d95bcdabd368731906d25bb32d5916e051c7f
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-10 13:26:49 +0000

    Add tailwind/ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,16 @@ export default function Home() {
         <title>Dev Jobs</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1 className='text-6xl font-bold'>Hello world!</h1>
+      <h1 className='text-6xl bg-green-100'>Hello world!</h1>
+      <p className=' text-2xl text-white bg-light-violet  px-2.5 py-1 m-2 rounded'>
+        Accent Color Text
+      </p>
       <footer className='flex items-center justify-center w-full h-24 border-t'>
         <a
           href='https://josephgattuso.github.io'
           className='flex items-center justify-center'
         >
-          Powerd by <img className='h-4 ml-2' src='/logo.svg' alt='' />
+          Powered by <img className='h-4 ml-2' src='/logo.svg' alt='' />
         </a>
       </footer>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,46 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    content: [
+      './src/components/**/*.{js,ts,jsx,tsx}',
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+    ],
+  },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: '#5964E0',
+        'light-violet': '#939BF4',
+        'very-dark-blue': '#19202D',
+        midnight: '#121721',
+        'light-grey': '#F4F6F8',
+        grey: '#9DAEC2',
+        'dark-grey': '#6E8098',
+      },
+      opacity: {
+        10: '0.1',
+        35: '0.35',
+      },
+      spacing: {
+        35: '8.75rem',
+      },
+      borderRadius: {
+        button: '0.3125rem',
+      },
+      lineHeight: {
+        button: '1.18rem',
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        brand: ['Kumbh Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  variants: {},
+  plugins: [require('@tailwindcss/ui')],
 };
