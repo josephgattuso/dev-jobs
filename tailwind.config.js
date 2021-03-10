21c81ff5cb7345aa3251b7310cb3a132a040eac8
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-10 17:02:41 +0000

    Update config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,12 @@ module.exports = {
     defaultLineHeights: true,
     standardFontWeights: true,
   },
+  experimental: {
+    darkModeVariant: true,
+  },
+  dark: 'class',
   purge: {
-    content: [
-      './src/components/**/*.{js,ts,jsx,tsx}',
-      './src/pages/**/*.{js,ts,jsx,tsx}',
-    ],
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
   },
   theme: {
     extend: {
@@ -23,29 +24,42 @@ module.exports = {
         'light-grey': '#F4F6F8',
         grey: '#9DAEC2',
         'dark-grey': '#6E8098',
+        'normal-grey': '#f2f2f2',
+        'checkbox-dark': 'rgba(255, 255, 255, 0.1)',
         logo: {
           1: '#DF6DAE',
           2: '#3DB3D1',
           3: '#3D3B94',
           4: '#F0B62A',
           5: '#E66D39',
-          6: '#222121',
-          7: '#5964E0',
-          8: '#FB7E66',
-          9: '#007CFF',
-          10: '#492A29',
-          11: '#60DCAD',
-          12: '#FF585F',
+          6: '#FB7E66',
+          7: '#007CFF',
+          8: '#492A29',
+          9: '#60DCAD',
+          10: '#FF585F',
         },
       },
       opacity: {
         10: '0.1',
+        20: '0.2',
         35: '0.35',
       },
       minHeight: {
         card: '14.25rem',
+        52: '13rem',
+        35: '8.75rem',
+      },
+      maxWidth: {
+        76: '19rem',
+        183: '45.75rem',
       },
       spacing: {
+        '1px': '0.0625rem',
+        3.5: '0.875rem',
+        10.5: '2.625rem',
+        14.5: '3.625rem',
+        30: '7.5rem',
+        34: '8.5rem',
         35: '8.75rem',
         76: '19rem',
       },
@@ -59,8 +73,13 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         brand: ['Kumbh Sans', ...defaultTheme.fontFamily.sans],
       },
+      flex: {
+        4: '4 4 0%',
+      },
     },
   },
-  variants: {},
+  variants: {
+    backgroundOpacity: ['responsive', 'hover', 'focus', 'dark'],
+  },
   plugins: [require('@tailwindcss/ui')],
 };
