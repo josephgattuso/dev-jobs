df937461cdca5c5d27e2dc62cde7edd016f04257
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-10 13:14:54 +0000

    Add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <>
-      <img src='/logo.svg' alt='' className='logo' />
-
-      <h1>Hello world!</h1>
-    </>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <Head>
+        <title>Dev Jobs</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <h1 className='text-6xl font-bold'>Hello world!</h1>
+      <footer className='flex items-center justify-center w-full h-24 border-t'>
+        <a
+          href='https://josephgattuso.github.io'
+          className='flex items-center justify-center'
+        >
+          Powerd by <img className='h-4 ml-2' src='/logo.svg' alt='' />
+        </a>
+      </footer>
+    </div>
   );
 }
