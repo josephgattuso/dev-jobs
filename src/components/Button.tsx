c390d8d330c23d4785ea52260fd92f2eb17c0e0f
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-11 11:48:46 +0000

    Add disabled state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,17 @@
 import { ReactNode } from 'react';
 import classNames from 'classnames';
-import JobView from './JobView';
 
 export default function Button({
+  disabled = false,
   className = '',
   children,
   primary = false,
   onClick = () => {},
   block = false,
 }: {
+  disabled?: boolean;
   className?: string;
-  children: ReactNode;
+  children?: ReactNode;
   primary?: boolean;
   onClick?: () => void;
   block?: boolean;
@@ -18,13 +19,15 @@ export default function Button({
   return (
     <>
       <button
+        disabled={disabled}
         className={classNames(
           className,
           'font-brand text-base font-bold leading-button h-12 bg-violet hover:bg-light-violet rounded-button',
           primary
             ? 'text-white'
             : 'text-violet dark:text-white dark:bg-white dark:bg-opacity-10 bg-opacity-10 hover:bg-opacity-35',
-          block ? 'w-full' : 'w-35'
+          block ? 'w-full' : 'w-35',
+          disabled && 'cursor-not-allowed opacity-50'
         )}
         onClick={onClick}
       >
