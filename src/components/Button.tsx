10d002eca6f476f77e23495ba4bced05124233cb
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-11 11:04:38 +0000

    Add responsive style to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,18 @@
 import { ReactNode } from 'react';
 import classNames from 'classnames';
+import JobView from './JobView';
 
 export default function Button({
   className = '',
   children,
   primary = false,
+  block = false,
   onClick = () => {},
 }: {
   className?: string;
   children: ReactNode;
   primary?: boolean;
+  block?: boolean;
   onClick?: () => void;
 }) {
   return (
@@ -17,10 +20,11 @@ export default function Button({
       <button
         className={classNames(
           className,
-          'font-brand font-bold text-base leading-button h-12 w-35 bg-violet hover:bg-light-violet rounded-button',
+          'font-brand font-bold text-base leading-button h-12 bg-violet hover:bg-light-violet rounded-button',
           primary
             ? 'text-white'
-            : 'text-violet bg-opacity-10 hover:bg-opacity-35'
+            : 'text-violet bg-opacity-10 hover:bg-opacity-35',
+          block ? 'w-full' : 'w-35'
         )}
         onClick={onClick}
       >
