7e4c58f1aeb7b9d713cc4028ba3cdcf2774c9670
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-11 10:46:14 +0000

    Update job view

--- src/components/JobView.tsx
+++ src/components/JobView.tsx
@@ -1,70 +1,63 @@
 import classNames from 'classnames';
 import { Job } from '../types';
-import { formatDate } from '../utils';
+import { formatDate, getRandomColor } from '../utils';
+import Link from 'next/link';
 
 export default function JobView({ job }: { job: Job }) {
-  const getColor = () => {
-    const number = Math.floor(Math.random() * 12 + 1);
-    switch (number) {
-      case 1:
-        return 'bg-logo-1';
-      case 2:
-        return 'bg-logo-2';
-      case 3:
-        return 'bg-logo-3';
-      case 4:
-        return 'bg-logo-4';
-      case 5:
-        return 'bg-logo-5';
-      case 6:
-        return 'bg-logo-6';
-      case 7:
-        return 'bg-logo-7';
-      case 8:
-        return 'bg-logo-8';
-      case 9:
-        return 'bg-logo-9';
-      case 10:
-        return 'bg-logo-10';
-      case 11:
-        return 'bg-logo-11';
-      case 12:
-        return 'bg-logo-12';
-      default:
-        return 'bg-black';
-    }
-  };
   return (
     <>
-      <div className='p-8 pt-0 mx-auto bg-white rounded-md w-76 sm:w-full min-h-card'>
+      <div className='relative p-8 pt-0 mx-auto bg-white rounded-md shadow dark:bg-very-dark-blue w-76 md:w-full min-h-card'>
         <div
           className={classNames(
-            'text-white font-brand font-bold absolute grid w-12 h-12 p-2 transform -translate-y-1/2 border place-items-center rounded-2xl',
-            getColor()
+            'text-white font-brand font-bold absolute grid w-12 h-12 p-2 transform -translate-y-1/2 place-items-center rounded-2xl',
+            getRandomColor()
           )}
         >
           {job.company[0]}
         </div>
-        <div className='flex items-center pt-12 text-base font-normal leading-5 text-dark-grey font-brand'>
+        <div className='flex items-center pt-12 text-base font-normal leading-5 text-dark-gray font-brand'>
           <p>{formatDate(new Date(job.created_at))} ago</p>
-          <p className='ml-6 text-4xl'>&middot;</p>
+          <p className='ml-3 text-4xl'>&middot;</p>
           <p className='ml-3'>{job.type}</p>
         </div>
         <div className='mt-3'>
-          <h2 className='text-lg font-bold leading-6 font-brand text-very-dark-blue'>
+          <h2 className='text-lg font-bold leading-6 font-brand text-very-dark-blue dark:text-white'>
             {job.title}
           </h2>
         </div>
         <div>
-          <p className='mt-3 text-base font-normal leading-5 font-brand text-dark-grey'>
+          <p className='mt-3 text-base font-normal leading-5 font-brand text-dark-gray'>
             {job.company}
           </p>
         </div>
         <div>
-          <p className='mt-10 text-sm font-bold font-brand text-black'>
+          <p className='mt-10 text-sm font-bold font-brand text-violet'>
             {job.location}
           </p>
         </div>
+        <Link href={`/jobs/${job.id}`}>
+          <div
+            title='View Details'
+            className='flex py-3 space-x-2 text-center cursor-pointer md:py-4 hover:bg-gray-100'
+          >
+            <svg
+              className='w-5 h-5 fill-current text-violet'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+            >
+              <path d='M10 12a2 2 0 100-4 2 2 0 000 4z' />
+              <path
+                fillRule='evenodd'
+                d='M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z'
+                clipRule='evenodd'
+              />
+            </svg>
+            <span className='text-sm font-semibold tracking-wider uppercase text-violet'>
+              View Details
+            </span>
+          </div>
+        </Link>
       </div>
     </>
   );
