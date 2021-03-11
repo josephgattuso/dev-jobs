0d6c5a1b82212cb3871ba75eb0050017ba7efb98
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-11 20:39:12 +0000

    Add modal component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 export { default as App } from './App';
 export { default as Button } from './Button';
-export { default as JobView } from './JobView';
 export { default as Input } from './Input';
-export { default as JobViewSkeleton } from './JobViewSkeleton';
 export { default as JobDetailsViewSkeleton } from './JobViewSkeleton';
+export { default as JobView } from './JobView';
+export { default as JobViewSkeleton } from './JobViewSkeleton';
+export { default as Modal } from './Modal';
