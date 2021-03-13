d1191d12a67c9ce2cb15884926eefa91d59dde19
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-13 10:40:02 +0000

    Add meta data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,42 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+          <meta charSet='utf-8' />
+          <meta name='application-name' content='DevJobs App' />
+          <meta name='twitter:card' content='summary_large_image' />
+          <meta name='twitter:creator' content='@joeetuso' />
+          <meta property='og:type' content='website' />
+          <meta name='author' content='Joseph Gattuso' />
+          <meta property='og:site_name' content='DevJobs App' />
+          <meta name='theme-color' content='#5964E0' />
+          <meta
+            name='description'
+            content='A DevJobs app using GitHub Jobs API.'
+          />
+          <meta property='og:title' content='DevJobs App' />
+          <meta
+            property='og:description'
+            content='A DevJobs app using GitHub Jobs API.'
+          />
+          <meta property='og:url' content='https://dev-jobs-api.vercel.app' />
+          <meta property='twitter:title' content='DevJobs' />
+          <meta
+            property='twitter:description'
+            content='A DevJobs app using GitHub Jobs API.'
+          />
+          <meta
+            property='twitter:url'
+            content='https://dev-jobs-api.vercel.app'
+          />
+          <meta
+            property='og:image'
+            content='https://dev-jobs-api.vercel.app/cover_light.png'
+          />
+          <meta
+            property='twitter:image'
+            content='https://dev-jobs-api.vercel.app/cover_light.png'
+          />
+          <link rel='shortcut icon' href='/favicon.ico' />
           <link
             href='https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;700&display=swap'
             rel='stylesheet'
